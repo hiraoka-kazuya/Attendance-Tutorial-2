7cb94894557a9e3227cf9ba1f01bd3596d5d11fc
hiraoka-kazuya <kazuya@email.com>
2021-07-11 23:04:11 +0000

    10章10.6終了

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20210704224907) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-07-03 23:00:00"
-    t.datetime "work_time", default: "2021-07-03 22:30:00"
+    t.datetime "basic_time", default: "2021-07-11 23:00:00"
+    t.datetime "work_time", default: "2021-07-11 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
