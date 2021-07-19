32d0fee1e2f05f8a380d125e4a226e360f9d5596
hiraoka-kazuya <kazuya@email.com>
2021-07-19 22:26:05 +0000

    11章1.2終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,7 +29,12 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-    debugger
   end
   
+  
+  private
+  #１ヶ月分の勤怠情報を扱います。
+  def attendance_params
+    paramus.require(:user).permit(attendances:[:started_at, :finished_at, :note])[:attendances]
+  end
 end
